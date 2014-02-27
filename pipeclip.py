6e9dee714fcf6f04b4cd303120514cd3dae3197d
sroos <sroos@sroos-mac-mini.dhcp.swmed.org>
2014-02-27 11:18:59 -0600

    refined the wrapper script

--- pipeclip.py
+++ pipeclip.py
@@ -1,3 +1,10 @@
+#Main pipeline connects all the scripts together
+#Original Programmer: beibei.chen@utsouthwestern.edu
+#Refactored by: eric.roos@utsouthwestern.edu
+#Usage: python pipeclip.py input.sam output_prefix match_length mismatch_number pcr_rm fdr_cluster clip_type fdr_mutation species
+#Required packages: pysam, ghmm, pybedtools
+#Last modification: 27 Feb 2014
+
 import sys
 import annotatePeaks
 import barcodeRemoval
@@ -8,32 +15,39 @@ import getCrosslinking
 import inputProcess
 import mergeReads
 import mutationFilter
-
+from subprocess import call
 
 if __name__=="__main__":
-  infile = sys.argv[1]
-  outputPrefix = sys.argv[2]
-  matchLength = sys.argv[3]
-  mismatch = sys.argv[4]
-  pcr = sys.argv[5]
-  fdrEnrichedCluster = sys.argv[6]
-  clipType =sys.argv[7]
-  fdrReliableMutation = sys.argv[8]
-  species = sys.argv[9]
+  infile = sys.argv[1]              # Input SAM/BAM file
+  outputPrefix = sys.argv[2]        # Output prefix
+  matchLength = sys.argv[3]         # Shorted matched segment length
+  mismatch = sys.argv[4]            # Maximum mismatch number
+  pcr = sys.argv[5]                 # PCR removal: (0)no removal; (1)same-start removal; (2)same-seq removal  
+  fdrEnrichedCluster = sys.argv[6]  # FDR for enriched clusters
+  clipType =sys.argv[7]             # CLIP type (0)HITS-CLIP; (1)PAR-4SU; (2)PAR-6SG; (3)iCLIP
+  fdrReliableMutation = sys.argv[8] # FDR for reliable mutations
+  species = sys.argv[9]             # Species ["mm10","hg19"]
 
+  ########################## Process input #######################
   inputProcessMain(infile,outputPrefix)
   SAMFILTERMain(outputPrefix+".sorted.bam",outputPrefix+".filter",matchLength,mismatch,pcr,clipType)
+
+  ######################### Enrich clusters ######################
   mergeReadsMain(outputPrefix+".filter.rehead.bam")
-  #./Rscript ZTNB.R outputPrefix+".filter.rehead.merge" fdrEnrichedCluster
+
+  ######################### Enrich clusters ######################
+  call(["./Rscript","ZTNB.R",outputPrefix+".filter.rehead.merge",fdrEnrichedCluster])
   getClusterMain(outputPrefix+".filter.rehead.merge",outputPrefix+".reheadmerge.ztnb", outputPrefix+".filter.cluster.bed")
 
+  ########################### Mutation #############################
   if clipType == "3":
     findTruncationMain(outputPrefix+".filter.rehead.bam")
   else
     findMutationMain(outputPrefix+".filter.rehead.bam",outputPrefix+".filter.rehead.merge.ztnb",clipType);
   mutationFilterMain(outputPrefix+".filter.rehead.bam",outputPrefix+".filter.mutation.bed",outputPrefix+".filter.reliable",clipType,fdrReliableMutation,outputPrefix+".filter.coverage")
 
-  if clipType == "7":
+  ######################### Merge and annotation ################
+  if clipType == "0":
     getCrossLinkingMain(outputPrefix+".filter.cluster.bed",outputPrefix+".reliable_deletion.bed", outputPrefix+"crosslinking.deletion.bed")
     getCrossLinkingMain(outputPrefix+".filter.cluster.bed",outputPrefix+".reliable_insertion.bed", outputPrefix+"crosslinking.insertion.bed")
     getCrossLinkingMain(outputPrefix+".filter.cluster.bed",outputPrefix+".reliable_substition.bed", outputPrefix+"crosslinking.substition.bed")
@@ -43,77 +57,5 @@ if __name__=="__main__":
         #annotatePeaks.pl $2.crosslinking.substitution.bed $9  > $2.crosslinking.substitution.anno.txt
     else
       getCrossLinkingMain(outputPrefix+".filter.cluster.bed",outputPrefix+".filter.reliable.bed",outputPrefix+".crosslinking.bed")
-      if ispecifies is not None
+      if species is not None
         #annotatePeaks.pl $2.crosslinking.bed $9  > $2.crosslinking.anno.txt
-
-"""
-#Main pipeline connects all the scripts together
-#Programmer: beibei.chen@utsouthwestern.edu
-#Usage: sh pipeclip.sh input.sam output_prefix match_length mismatch_number pcr_rm fdr_cluster clip_type fdr_mutation species
-#Required packages: pysam, ghmm, pybedtools, samtools
-#Required parameters
-#$1: Input SAM/BAM file
-#$2: Output prefix
-#$3: Shorted matched segment length
-#$4: Maximum mismatch number
-#$5: PCR removal: (0)no removal; (1)same-start removal; (2)same-seq removal  
-#$6: FDR for enriched clusters
-#$7: CLIP type (0)HITS-CLIP; (1)PAR-4SU; (2)PAR-6SG; (3)iCLIP
-#$8: FDR for reliable mutations
-#$9: Species ["mm10","hg19"]
-#
-#Last modification: 19 Dec 2013
-#
-##########################Process input#######################
-python inputProcess.py $1 $2  #output $2.sorted.bam
-""
-""
-samtools index $2.sorted.bam
-samtools view -H $2.sorted.bam > $2.header
-python SAMFilter.py -i $2.sorted.bam -o $2.filter -m $3 -n $4 -r $5 -t $7
-samtools reheader $2.header $2.filter.bam > $2.filter.rehead.bam
-samtools index $2.filter.rehead.bam
-#
-#
-#########################Clustering filtered mapped reads######
-python mergeReads.py $2.filter.rehead.bam > $2.filter.rehead.merge
-#
-#
-#########################Enrich clusters######################
-./Rscript ZTNB.R $2.filter.rehead.merge $6
-python getCluster.py $2.filter.rehead.merge $2.filter.rehead.merge.ztnb > $2.filter.cluster.bed
-
-
-#BC##########################Mutation#############################
-if test "$7" = "3"  #iCLIP
-  then
-    python findTruncation.py $2.filter.rehead.bam > $2.filter.mutation.bed
-  else
-    python findMutation.py -i $2.filter.rehead.bam -o $2.filter.mutation.bed -p $7
-fi
-python mutationFilter.py -a $2.filter.rehead.bam -b $2.filter.mutation.bed -o $2.filter.reliable -p $7 -f $8 -c $2.filter.coverage
-#
-#
-#########################Merge and annotation################
-if test "$7" = "0" #HITS-CLIP
-  then
-  python getCrosslinking.py $2.filter.cluster.bed $2.filter.reliable_deletion.bed > $2.crosslinking.deletion.bed
-  python getCrosslinking.py $2.filter.cluster.bed $2.filter.reliable_insertion.bed > $2.crosslinking.insertion.bed
-  python getCrosslinking.py $2.filter.cluster.bed $2.filter.reliable_substitution.bed > $2.crosslinking.substitution.bed
-#  echo "HITS finish"
-  if test "$9" != "Null"
-    then
-      annotatePeaks.pl $2.crosslinking.deletion.bed $9 > $2.crosslinking.deletion.anno.txt
-      annotatePeaks.pl $2.crosslinking.insertion.bed $9 > $2.crosslinking.insertion.anno.txt
-      annotatePeaks.pl $2.crosslinking.substitution.bed $9  > $2.crosslinking.substitution.anno.txt
-  fi
-  else
-    python getCrosslinking.py $2.filter.cluster.bed $2.filter.reliable.bed > $2.crosslinking.bed
-    if test "$9" != "Null"
-      then
-        annotatePeaks.pl $2.crosslinking.bed $9  > $2.crosslinking.anno.txt
-    fi
-fi
-#
-"""
-
