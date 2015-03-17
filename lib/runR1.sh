b7221975fc69d34bdbf12dfb6ab7c298f127f110
Beibei Chen  Qbri <beibei.chen@utsouthwestern.edu>
2015-03-17 09:27:45 -0500

    Use shell slave

---
+++ lib/runR1.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+filename="test" 
+#pvalue = $2
+#declare -a epsilon
+#declare -a steps
+epsilon=(0.01 0.15 0.1)
+steps=(0.1 0.08 0.05)
+#sleep 500
+#status=0
+
+#check if file is generated
+#while ["$status" == "0"]
+#do
+#	if [-p "$filename"]
+#		then
+#			status = 1
+#			filesize = $(stat -c%s "$filename")
+#		else
+#			sleep 60
+#		fi
+#done
+#
+##make sure file does not change
+#filestat = 0
+#while ["$filestat"=="0"]
+#do
+#	currentsize = $(stat -c%s "$filename")
+#	if ["$filesize" == "$currentsize"]
+#		filestat = 1
+#	else
+#		sleep 60
+#	fi
+#done
+
+#Call R function
+r_status="1"
+count=1
+for e in "${epsilon[@]}"
+do
+	for s in "${steps[@]}"
+	do
+		if [ -p "Converge.txt" ]
+		then
+			echo
+		else
+			#echo "$e,$s"
+			/home/bchen4/Download/R-3.1.1/bin/Rscript ZTNB_tryCatch.R $filename 0.01 $e $s
+		fi
+		#echo "$filename.$count"
+		count=$((count+1))
+	done
+done
+
+
